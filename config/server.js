fb2df2a2308b38cd2c557c5149a1881dd7447905
Sangram Rath <sangram@cloudwrks.in>
2023-01-07 07:52:44 +0530

    Added dummy keys

--- config/server.js
+++ config/server.js
@@ -1,7 +1,12 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'tobemodified'),
+    },
+  },
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', 'toBeModified1,toBeModified2'),
   },
 });
